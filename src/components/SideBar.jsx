f81045422d848628cb93d97b000054e109b8e97a
Tony Michael <tee.jhay1@gmail.com>
2024-02-26 15:30:03 +0100

    side bar glitch fixed

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,7 +1,7 @@
 "use client"
 import { motion } from "framer-motion";
 import Image from "next/image";
-import { useState } from "react";
+import { useLayoutEffect, useState } from "react";
 import { IoMdClose } from "react-icons/io";
 import { LuListMinus } from "react-icons/lu";
 
@@ -11,28 +11,35 @@ const variants = {
   closed: { opacity: 0, x: "-100%" },
 }
 
-
+// side bar
 const SideBar = () => {
   const [open, setOpen] = useState(false)
   const toggleSideBar = () => {
     setOpen(prev => !prev)
   }
+  useLayoutEffect(() => {
+    setOpen(false)
+  }, [])
+  // ! fix the side bar opening transition error
   return (
     <div className="md:hidden" >
       <LuListMinus className="cursor-pointer" size={30} onClick={toggleSideBar} />
-      <motion.nav
-        animate={open ? "open" : "closed"}
-        variants={variants}
-        className="h-screen top-0 left-0 z-40 absolute bg-black bg-opacity-25 w-full">
-        <div className="h-screen w-4/5 border-y border-y-gray-200 bg-white"  >
-          <div className="flex justify-between items-center border-b border-b-gray-200 py-2.5 px-5  ">
-            <Image src={'/takiaLogo.png'} width={80} height={70} />
-            <div className="rounded-full border cursor-pointer border-gray-300 p-1.5 " onClick={toggleSideBar} >
-              <IoMdClose size={23} className="text-gray-800" />
+
+        <motion.nav
+          initial={{ opacity: 0 }}
+          animate={open ? "open" : "closed"}
+          variants={variants}
+          className="h-screen top-0 left-0 z-40 absolute bg-black bg-opacity-25 w-full">
+          <div className="h-screen w-4/5 border-y border-y-gray-200 bg-white"  >
+            <div className="flex justify-between items-center border-b border-b-gray-200 py-2.5 px-5  ">
+              <Image src={'/takiaLogo.png'} width={80} height={70} />
+              <div className="rounded-full border cursor-pointer border-gray-300 p-1.5 " onClick={toggleSideBar} >
+                <IoMdClose size={23} className="text-gray-800" />
+              </div>
             </div>
           </div>
-        </div>
-      </motion.nav>
+        </motion.nav>
+      
     </div>
   );
 }
