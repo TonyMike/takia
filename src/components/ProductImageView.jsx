343d2bdc98f14870abcf5449869ede1a556a7d2e
Tony Michael <tee.jhay1@gmail.com>
2024-03-03 15:20:46 +0100

    image slider

--- src/components/ProductImageView.jsx
+++ src/components/ProductImageView.jsx
@@ -3,14 +3,20 @@
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import 'swiper/css';
+import 'swiper/css/free-mode';
 import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css/thumbs';
+import "../../src/app/globals.css";
+
+
+import { FreeMode, Navigation, Thumbs } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 const ProductImageView = ({ productId }) => {
-  console.assert(productId)
   const [product, setProduct] = useState(null)
+  const [thumbsSwiper, setThumbsSwiper] = useState(null)
+  const [direction, setDirection] = useState('');
+
   useEffect(() => {
     const fetchProduct = async () => {
       try {
@@ -25,20 +31,41 @@ const ProductImageView = ({ productId }) => {
     fetchProduct()
   }, [])
 
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 768) {
+        console.log('window width is less than 768')
+        setDirection('vertical')
+      } else {
+        console.log('window width is greater than 768')
+        setDirection('horizontal')
+      }
+    }
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    }
+  }, []);
+
+
+
 
   return (
     <div className="grid grid-cols-1 gap-2 md:gap-4 md:grid-cols-6 md:grid-flow-col-dense">
       <div className="h-72 bg-gray-100 rounded-md md:col-span-5 md:h-96 lg:h-[450px] xl:h-[500px] ">
         <Swiper
-          modules={[Navigation, Pagination]}
+          modules={[FreeMode, Navigation, Thumbs]}
           className=" h-full tetx-sm"
-          spaceBetween={50}
+          spaceBetween={13}
           slidesPerView={1}
-          // scrollbar={{ draggable: true }}
-          pagination={{ type: 'fraction' }}
           navigation={true}
           onSlideChange={() => console.log('slide change')}
           onSwiper={(swiper) => console.log(swiper)}
+          thumbs={{
+            swiper: thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null
+          }}
         >
           {
             product?.images.map((image, index) => {
@@ -49,28 +76,34 @@ const ProductImageView = ({ productId }) => {
               </SwiperSlide>
             })
           }
-          {/* <SwiperSlide>Slide 1</SwiperSlide>
-          <SwiperSlide>Slide 2</SwiperSlide>
-          <SwiperSlide>Slide 3</SwiperSlide>
-          <SwiperSlide>Slide 4</SwiperSlide> */}
           ...
         </Swiper>
       </div>
-      <div className="h-20  md:order-first md:col-span-1 md:h-96 lg:h-[450px] xl:h-[500px] grid  grid-cols-4 grid-rows-1 md:grid-cols-1 md:grid-rows-4 md:grid-flow-col-dense gap-2  ">
-        {
-          // Array(4).fill(0).map((x, i) => {
-          //   return <div className="bg-gray-100 rounded-md relative" key={i}>{i}</div>
-          // })
-          product?.images.map((x, i) => {
-            return <div className="bg-gray-100 overflow-hidden shadow-md rounded-md relative" key={i}>
-              <Image src={x} alt={x} layout="fill" objectFit="cover" />
-            </div>
-          })
-        }
-
+      <div className="h-20 md:h-[450px] ">
+        {/* md:order-first md:col-span-1 md:h-96 lg:h-[450px] xl:h-[500px] grid  grid-cols-4 grid-rows-1 md:grid-cols-1 md:grid-rows-4 md:grid-flow-col-dense gap-2 */}
+        <Swiper
+          className="   w-full h-full"
+          onSwiper={setThumbsSwiper}
+          spaceBetween={10}
+          slidesPerView={4}
+          direction={direction}
+          freeMode={true}
+          watchSlidesProgress={true}
+          modules={[FreeMode, Navigation, Thumbs]}
+        >
+          {
+            product?.images.map((x, i) => {
+              return <SwiperSlide className="h-full overflow-hidden w-full rounded-lg relative bg-green-800  " key={i} >
+                <Image src={x} alt={x} className="" layout="fill" objectFit="cover" />
+              </SwiperSlide>
+            })
+          }
+        </Swiper>
       </div>
+
     </div>
   );
 }
+
 
 export default ProductImageView;
