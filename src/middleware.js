7d080e0db2d007de3e2c27d79e999c56b162dafd
Tony Michael <tee.jhay1@gmail.com>
2024-03-10 15:12:46 +0100

    fixing bug

--- src/middleware.js
+++ src/middleware.js
@@ -4,5 +4,10 @@ import { authConfig } from "./lib/auth.config"
 export default NextAuth(authConfig).auth
 
 export const config = {
+  runtime: 'edge', // for Edge API Routes only
+  unstable_allowDynamic: [
+    // '/lib/utils.js', // allows a single file
+    '/node_modules/function-bind/**', // use a glob to allow anything in the function-bind 3rd party module
+  ],
   matcher: ['/((?!api|static|.*\\..*|_next).*)'],
 }
