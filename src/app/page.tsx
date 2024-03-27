970de087c0789a6b8c3e410c21787680a69128e8
Tony Michael <tee.jhay1@gmail.com>
2024-03-27 08:39:53 +0100

    vercel bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ import { getAllProducts } from "../lib/actions";
 import "./globals.css";
 
 export default async function Home() {
-  const allProducts = await getAllProducts();
-  const serializedData = JSON.parse(JSON.stringify(allProducts));
+  const allProducts= await getAllProducts();
+  const serializedData: any= JSON.parse(JSON.stringify(allProducts));
   return (
     <div className="">
 
